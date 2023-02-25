4d2a6afc6f71fa24be81fa259b2eba9a1d9e8af2
J <jamscc22@gmail.com>
2023-02-24 22:52:10 -0500

    footer section and css - draft

--- src/components/sections/Footer-Section.js
+++ src/components/sections/Footer-Section.js
@@ -1,6 +1,15 @@
+import { Container, Navbar } from 'react-bootstrap';
+import '../css/style.css';
+
+// footer
 function FooterSection() {
     return (
         <>
+            <Navbar fixed="bottom" bg="dark" variant="dark">
+                <Container className="d-flex justify-content-center">
+                    <Navbar.Brand href="https://github.com/jamscc" target="_blank" rel="noreferrer" className="footer-links"> GitHub Profile</Navbar.Brand>
+                </Container>
+            </Navbar>
         </>
     );
 }
