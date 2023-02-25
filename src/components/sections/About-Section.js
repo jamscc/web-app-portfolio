1b004601bb2c4d4cd3be968c983d92a519e7675d
J <jamscc22@gmail.com>
2023-02-25 00:11:05 -0500

    about section and css - draft

--- src/components/sections/About-Section.js
+++ src/components/sections/About-Section.js
@@ -1,6 +1,22 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import img from './images/avatar.png';
+import '../css/style.css';
+
+// about section
 function AboutSection() {
     return (
-        <div>
+        <div className="about-mq">
+            <Container className="about-container">
+                <Row>
+                    <Col fluid="sm" sm={12} className="d-flex justify-content-center">
+                        {/* source: Avatar Maker from Chrome Web Store */}
+                        <img id="about-img" fluid="sm" src={img} alt="avatar"></img>
+                    </Col>
+                    <Col fluid="sm" sm={12} className="d-flex justify-content-center">
+                        <p id="about-text">Welcome to my coding portfolio! My name is James. Most of my professional experience has been focused on real estate, and in this field, I have enjoyed using various apps and tools. Over the years, I have become increasingly interested in coding, and so, I decided to enroll in a coding bootcamp. Please visit the Portfolio section, which includes some samples of my portfolio that I have put together to date while in the program. Thank you for visiting my page. Hope to hear from you soon!</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
